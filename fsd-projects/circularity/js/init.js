6ce8527320f66f2f5aca2c7156026ac8fd234928
Kamryn <kamrynerouse@gmail.com>
2025-04-08 17:13:56 +0000

    completed bouncing box project

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -20,7 +20,7 @@ var init = function (window) {
         ///////////////////
         
         // TODO 1 : Declare and initialize our variables
-
+        var circles = [];
 
 
         // TODO 2 : Create a function that draws a circle 
